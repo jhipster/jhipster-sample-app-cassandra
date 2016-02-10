e2fa6f8bf1dd2eb3a97da4b00f3edf5a2e9a86b2
Julien Dubois <julien.dubois@gmail.com>
2016-02-10 10:27:19 +0100

    automatic project update

--- src/main/webapp/scripts/components/form/pager.directive.js
+++
@@ -1,9 +0,0 @@
-/* globals $ */
-'use strict';
-
-angular.module('sampleCassandraApp')
-    .directive('sampleCassandraAppPager', function() {
-        return {
-            templateUrl: 'scripts/components/form/pager.html'
-        };
-    });
