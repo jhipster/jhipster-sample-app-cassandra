5b04b57bc3fee1773aa2c67c22e3565ca74f0eef
Julien Dubois <julien.dubois@gmail.com>
2016-05-27 11:50:21 +0200

    automatic project update

--- src/main/webapp/app/layouts/navbar/active-menu.directive.js
+++ src/main/webapp/app/layouts/navbar/active-menu.directive.js
@@ -5,6 +5,8 @@
         .module('jhipsterCassandraSampleApplicationApp')
         .directive('activeMenu', activeMenu);
 
+    activeMenu.$inject = ['$translate', '$locale', 'tmhDynamicLocale'];
+
     function activeMenu($translate, $locale, tmhDynamicLocale) {
         var directive = {
             restrict: 'A',
--- src/main/webapp/app/services/auth/has-authority.directive.js
+++ src/main/webapp/app/services/auth/has-authority.directive.js
@@ -5,6 +5,8 @@
         .module('jhipsterCassandraSampleApplicationApp')
         .directive('hasAuthority', hasAuthority);
 
+    hasAuthority.$inject = ['Principal'];
+
     function hasAuthority(Principal) {
         var directive = {
             restrict: 'A',
