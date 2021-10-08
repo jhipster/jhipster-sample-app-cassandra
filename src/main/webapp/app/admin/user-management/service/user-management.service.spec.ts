381b6ab365f24f6a7f46b224b2bf6481ac31472d
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-10-08 14:57:38 +0200

    automatic project update

--- src/main/webapp/app/admin/user-management/service/user-management.service.spec.ts
+++ src/main/webapp/app/admin/user-management/service/user-management.service.spec.ts
@@ -6,51 +6,49 @@ import { User } from '../user-management.model';
 
 import { UserManagementService } from './user-management.service';
 
-describe('Service Tests', () => {
-  describe('User Service', () => {
-    let service: UserManagementService;
-    let httpMock: HttpTestingController;
+describe('User Service', () => {
+  let service: UserManagementService;
+  let httpMock: HttpTestingController;
 
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule],
-      });
-
-      service = TestBed.inject(UserManagementService);
-      httpMock = TestBed.inject(HttpTestingController);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
     });
 
-    afterEach(() => {
-      httpMock.verify();
+    service = TestBed.inject(UserManagementService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('Service methods', () => {
+    it('should return User', () => {
+      let expectedResult: string | undefined;
+
+      service.find('user').subscribe(received => {
+        expectedResult = received.login;
+      });
+
+      const req = httpMock.expectOne({ method: 'GET' });
+      req.flush(new User('ABC', 'user'));
+      expect(expectedResult).toEqual('user');
     });
 
-    describe('Service methods', () => {
-      it('should return User', () => {
-        let expectedResult: string | undefined;
+    it('should propagate not found response', () => {
+      let expectedResult = 0;
 
-        service.find('user').subscribe(received => {
-          expectedResult = received.login;
-        });
-
-        const req = httpMock.expectOne({ method: 'GET' });
-        req.flush(new User('ABC', 'user'));
-        expect(expectedResult).toEqual('user');
+      service.find('user').subscribe({
+        error: (error: HttpErrorResponse) => (expectedResult = error.status),
       });
 
-      it('should propagate not found response', () => {
-        let expectedResult = 0;
-
-        service.find('user').subscribe({
-          error: (error: HttpErrorResponse) => (expectedResult = error.status),
-        });
-
-        const req = httpMock.expectOne({ method: 'GET' });
-        req.flush('Invalid request parameters', {
-          status: 404,
-          statusText: 'Bad Request',
-        });
-        expect(expectedResult).toEqual(404);
+      const req = httpMock.expectOne({ method: 'GET' });
+      req.flush('Invalid request parameters', {
+        status: 404,
+        statusText: 'Bad Request',
       });
+      expect(expectedResult).toEqual(404);
     });
   });
 });
